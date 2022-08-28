2b538088aa841233b65e311688d27129edde6855
Madhav Kandukuri <madhav165@gmail.com>
2022-08-28 16:44:29 +0530

    audio transformers

--- basic/main.py
+++ basic/main.py
@@ -13,8 +13,8 @@ results = classifier(["We are very happy to show you the 🤗 Transformers libra
 for result in results:
     print(f"label: {result['label']}, with score: {round(result['score'], 4)}")
 
-# speech_recognizer = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
-# dataset = load_dataset("PolyAI/minds14", name="en-US", split="train")
-# dataset = dataset.cast_column("audio", Audio(sampling_rate=speech_recognizer.feature_extractor.sampling_rate))
-# result = speech_recognizer(dataset[:4]["audio"])
-# print([d["text"] for d in result])
+speech_recognizer = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
+dataset = load_dataset("PolyAI/minds14", name="en-US", split="train")
+dataset = dataset.cast_column("audio", Audio(sampling_rate=speech_recognizer.feature_extractor.sampling_rate))
+result = [speech_recognizer(x["array"]) for x in dataset[:4]["audio"]]
+print([d["text"] for d in result])
