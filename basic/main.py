7edbf4edbb5befe0fdedcdb4a919baf4c59cdb9b
Madhav Kandukuri <madhav165@gmail.com>
2022-08-28 17:18:49 +0530

    code for automodel and saving model

--- basic/main.py
+++ basic/main.py
@@ -1,8 +1,9 @@
 import os
+from torch import nn
 os.environ['TRANSFORMERS_CACHE'] = os.path.join(os.getcwd(), str('/'.join(['models','pretrained'])))
 os.environ['HF_HOME'] = os.path.join(os.getcwd(), str('/'.join(['datasets','prebuilt'])))
 
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 from datasets import load_dataset, Audio
 
 classifier = pipeline("sentiment-analysis")
@@ -18,3 +19,30 @@ dataset = load_dataset("PolyAI/minds14", name="en-US", split="train")
 dataset = dataset.cast_column("audio", Audio(sampling_rate=speech_recognizer.feature_extractor.sampling_rate))
 result = [speech_recognizer(x["array"]) for x in dataset[:4]["audio"]]
 print([d["text"] for d in result])
+
+model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+result = classifier("Nous sommes très heureux de vous présenter la bibliothèque 🤗 Transformers.")
+print(f"label: {result[0]['label']}, with score: {round(result[0]['score'], 4)}")
+
+model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+pt_batch = tokenizer(
+    ["We are very happy to show you the 🤗 Transformers library.", "We hope you don't hate it."],
+    padding=True,
+    truncation=True,
+    max_length=512,
+    return_tensors="pt",
+)
+pt_model = AutoModelForSequenceClassification.from_pretrained(model_name)
+pt_outputs = pt_model(**pt_batch)
+pt_predictions = nn.functional.softmax(pt_outputs.logits, dim=-1)
+print(pt_predictions)
+
+pt_save_directory = "./pt_save_pretrained"
+tokenizer.save_pretrained(pt_save_directory)
+pt_model.save_pretrained(pt_save_directory)
+pt_model = AutoModelForSequenceClassification.from_pretrained("./pt_save_pretrained")
