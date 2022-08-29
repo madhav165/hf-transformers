1122463743020d84fee72f71922bcb72052d3912
Madhav Kandukuri <Madhav.Kandukuri@ibm.com>
2022-08-29 12:56:36 +0530

    added comments

--- basic/main.py
+++ basic/main.py
@@ -6,6 +6,8 @@ os.environ['HF_HOME'] = os.path.join(os.getcwd(), str('/'.join(['datasets','preb
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 from datasets import load_dataset, Audio
 
+# Classifier
+
 classifier = pipeline("sentiment-analysis")
 result = classifier("We are very happy to show you the 🤗 Transformers library.")
 print(f"label: {result[0]['label']}, with score: {round(result[0]['score'], 4)}")
@@ -14,11 +16,15 @@ results = classifier(["We are very happy to show you the 🤗 Transformers libra
 for result in results:
     print(f"label: {result['label']}, with score: {round(result['score'], 4)}")
 
+# Audio
+
 speech_recognizer = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
 dataset = load_dataset("PolyAI/minds14", name="en-US", split="train")
 dataset = dataset.cast_column("audio", Audio(sampling_rate=speech_recognizer.feature_extractor.sampling_rate))
 result = [speech_recognizer(x["array"]) for x in dataset[:4]["audio"]]
 print([d["text"] for d in result])
+
+# Multi lingual
 
 model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
@@ -27,6 +33,8 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 result = classifier("Nous sommes très heureux de vous présenter la bibliothèque 🤗 Transformers.")
 print(f"label: {result[0]['label']}, with score: {round(result[0]['score'], 4)}")
+
+# Automodel
 
 model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
