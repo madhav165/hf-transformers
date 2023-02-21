14bde22e2e93f2b6325da459e926fc3fc7182131
Madhav Kandukuri <Madhav.Kandukuri@ibm.com>
2023-02-21 13:26:04 +0530

    working parser

--- homoeo/parser.py
+++ homoeo/parser.py
@@ -9,8 +9,18 @@ soup = BeautifulSoup(resp.content)
 text = soup.text
 text = re.sub(' +', ' ', text)
 text = re.sub('\n ', ' ', text)
-text = re.sub('\n\n', '\t', text)
-text = re.sub('\n', ' ', text)
-text = re.sub('\t', '\n', text)
-with open ('ars.txt', 'w', encoding='utf-8') as f:
-    f.write(text)
+
+# text = re.sub('\n\n', '\t', text)
+# text = re.sub('\n', ' ', text)
+# text = re.sub('\t', '\n', text)
+clinical = re.search('Clinical\.─((.|\n)*)(?=Characteristics\.─)', text).group(0)
+clinical = re.sub('\n', ' ', clinical).strip()
+characteristics = re.search('Characteristics\.─((.|\n)*)(?=Relations\.─)', text).group(0)
+characteristics = re.sub('\n', ' ', characteristics).strip()
+relations = re.search('Relations\.─((.|\n)*)(?=Causation\.─)', text).group(0)
+relations = re.sub('\n', ' ', relations).strip()
+causation = re.search('Causation\.─((.|\n)*)(?=SYMPTOMS\.)', text).group(0)
+causation = re.sub('\n', ' ', causation).strip()
+
+# with open ('ars.txt', 'w', encoding='utf-8') as f:
+#     f.write(text)
